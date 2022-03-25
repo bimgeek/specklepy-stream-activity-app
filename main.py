166a8bbfe0e3d919be77b7ea57ce9596f0ed538d
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2022-03-25 15:08:11 +0300

    added comments in the code

--- main.py
+++ main.py
@@ -1,76 +1,87 @@
-#import libraries
+#--------------------------
+#IMPORT LIBRARIES
 #import streamlit
-from re import X
 import streamlit as st
 #specklepy libraries
 from specklepy.api.client import SpeckleClient
 from specklepy.api.credentials import get_account_from_token
 #import pandas
 import pandas as pd
-#plotly express. fast and easy way to generate graphs.
+#import plotly express
 import plotly.express as px
+#--------------------------
 
-#page config
+#--------------------------
+#PAGE CONFIG
 st.set_page_config(
     page_title="Speckle Stream Activity",
     page_icon="📊"
 )
-#header container
+#--------------------------
+
+#--------------------------
+#CONTAINERS
 header = st.container()
-#header
+input = st.container()
+viewer = st.container()
+report = st.container()
+graphs = st.container()
+#--------------------------
+
+#--------------------------
+#HEADER
+#Page Header
 with header:
     st.title("Speckle Stream Activity App📈")
-
-with st.expander("About this app📣", expanded=True):
-
+#About info
+with header.expander("About this app🔽", expanded=True):
     st.markdown(
         """This is a beginner web app developed using Streamlit. My goal was to understand how to interact with Speckle API using SpecklePy, 
         analyze what is received and its structure. This was easy and fun experiment.
         """
     )
+#--------------------------
 
-#--------------------
-#input and report containers
-input = st.container()
-viewer = st.container()
-report = st.container()
-graphs = st.container()
-
-#--------------------
-
-#lets get inputs
+#--------------------------
+#INPUTS
 with input:
-    st.subheader("Inputs...")
-    #3 columns for 3 inputs
+    st.subheader("Inputs")
+
+    #-------
+    #Columns for inputs
     serverCol, tokenCol = st.columns([1,3])
-    #input variables
+    #User Input boxes
     speckleServer = serverCol.text_input("Server URL", "speckle.xyz", help="Speckle server to connect.")
     speckleToken = tokenCol.text_input("Speckle token", "087fea753d12f91a6f692c8ea087c1bf4112e93ed7", help="If you don't know how to get your token, take a look at this [link](https://speckle.guide/dev/tokens.html)👈")
+    #-------
 
-    #--------------
-    #Speckle Client
+    #-------
+    #CLIENT
     client = SpeckleClient(host=speckleServer)
-    #get account from token
+    #Get account from Token
     account = get_account_from_token(speckleToken, speckleServer)
-    #authenticate using the account
+    #Authenticate
     client.authenticate_with_account(account)
-    #--------------------------------
+    #-------
 
-    #get list of streams
+    #-------
+    #Streams List👇
     streams = client.stream.list()
-    #get stream names
+    #Get Stream Names
     streamNames = [s.name for s in streams]
-    #create dropdown for user to select stream name
+    #Dropdown for stream selection
     sName = st.selectbox(label="Select your stream", options=streamNames, help="Select your stream from the dropdown")
-    #get stream from name
+    #SELECTED STREAM ✅
     stream = client.stream.search(sName)[0]
-    #st.write(stream)
-    #list commits
-    scommits = client.commit.list(stream.id,limit=100)
-    #list branches
+    #Stream Branches 🌴
     branches = client.branch.list(stream.id)
-    #-------------
+    #Stream Commits 🏹
+    commits = client.commit.list(stream.id, limit=100)
+    #-------
+#--------------------------
 
+#--------------------------
+#DEFINITIONS
 #create a definition to convert your list to markdown
 def listToMarkdown(list, column):
     list = ["- " + i + " \n" for i in list]
@@ -81,56 +92,89 @@ def listToMarkdown(list, column):
 def commit2viewer(stream, commit, height=400) -> str:
     embed_src = "https://speckle.xyz/embed?stream="+stream.id+"&commit="+commit.id
     return st.components.v1.iframe(src=embed_src, height=height)
+#--------------------------
 
-#Speckle Viewer
+#--------------------------
+#VIEWER👁‍🗨
 with viewer:
     st.subheader("Latest Commit👇")
-    commit2viewer(stream, scommits[0])
+    commit2viewer(stream, commits[0])
+#--------------------------
 
-#OUTPUTs
+#--------------------------
+#REPORT
 with report:
     st.subheader("Statistics")
-    
-    #4 columns for 4 Cards
+
+    #-------
+    # Columns for Cards
     branchCol, commitCol, connectorCol, contributorCol = st.columns(4)
-    #-----------------
-    #Branch Column
-    branchCol.metric(label = "Number of branches", value= len(branches))
+    #-------
+
+    #-------
+    #Branch Card 💳
+    branchCol.metric(label = "Number of branches", value= stream.branches.totalCount)
     #branch names as markdown list
     branchNames = [b.name for b in branches]
     listToMarkdown(branchNames, branchCol)
+    #-------
 
-    #Commit Column
-    commitCol.metric(label = "Number of commits", value= len(scommits))
+    #-------
+    #Commit Card 💳
+    commitCol.metric(label = "Number of commits", value= len(commits))
+    #-------
 
-    #Connector Column
+    #-------
+    #Connector Card 💳
     #connector list
-    connectorList = [c.sourceApplication for c in scommits]
+    connectorList = [c.sourceApplication for c in commits]
     #number of connectors
-    connectorCol.metric(label="Number of connectors", value= len(dict.fromkeys([c.sourceApplication for c in scommits])))
+    connectorCol.metric(label="Number of connectors", value= len(dict.fromkeys(connectorList)))
     #get connector names
     connectorNames = list(dict.fromkeys(connectorList))
     #convert it to markdown list
     listToMarkdown(connectorNames, connectorCol)
+    #-------
 
-    #Contributor Column
+    #-------
+    #Contributor Card 💳
+    contributorCol.metric(label = "Number of contributors", value= len(stream.collaborators))
     #unique contributor names
     contributorNames = list(dict.fromkeys([col.name for col in stream.collaborators]))
-    #number of contributors
-    contributorCol.metric(label = "Number of collaborators", value= len(contributorNames))
-
     #convert it to markdown list
-    listToMarkdown(contributorNames,contributorCol)    
+    listToMarkdown(contributorNames,contributorCol)
+    #-------
 
+#--------------------------
+
+#--------------------------
 with graphs:
     st.subheader("Graphs")
+    #COLUMNS FOR CHARTS
     branch_graph_col, connector_graph_col, collaborator_graph_col = st.columns([2,1,1])
-    #-------------------------
-    #CONNECTOR CHART
-    #commits to dataframe
-    scommits = pd.DataFrame.from_dict([c.dict() for c in scommits])
+    
+    #-------
+    #BRANCH GRAPH 📊
+    #branch count dataframe
+    branch_counts = pd.DataFrame([[branch.name, branch.commits.totalCount] for branch in branches])
+    #rename dataframe columns
+    branch_counts.columns = ["branchName", "totalCommits"]
+    #create graph
+    branch_count_graph = px.bar(branch_counts, x=branch_counts.branchName, y=branch_counts.totalCommits, color=branch_counts.branchName, labels={"branchName":"","totalCommits":""})
+    #update layout
+    branch_count_graph.update_layout(
+        showlegend = False,
+        margin = dict(l=1,r=1,t=1,b=1),
+        height=220)
+    #show graph
+    branch_graph_col.plotly_chart(branch_count_graph, use_container_width=True)
+    #-------
+
+    #-------
+    #CONNECTOR CHART 🍩
+    commits= pd.DataFrame.from_dict([c.dict() for c in commits])
     #get apps from commits
-    apps = scommits["sourceApplication"]
+    apps = commits["sourceApplication"]
     #reset index
     apps = apps.value_counts().reset_index()
     #rename columns
@@ -146,11 +190,12 @@ with graphs:
         )
     #set width of the chart so it uses column width
     connector_graph_col.plotly_chart(fig, use_container_width=True)
-    #-------------------------
-    #-------------------------
-    #COLLABORATOR CHART
+    #-------
+
+    #-------
+    #COLLABORATOR CHART 🍩
     #get authors from commits
-    authors = scommits["authorName"].value_counts().reset_index()
+    authors = commits["authorName"].value_counts().reset_index()
     #rename columns
     authors.columns=["author","count"]
     #create our chart
@@ -160,27 +205,14 @@ with graphs:
         margin=dict(l=1,r=1,t=1,b=1),
         height=200,
         yaxis_scaleanchor="x",)
-    collaborator_graph_col.plotly_chart(authorFig, use_container_width=True)
-    #-------------------------
-
-    #BRANCH ACTIVITY
-    #-------------------------
-    branch_counts = pd.DataFrame([[branch.name, branch.commits.totalCount] for branch in branches])
-    branch_counts.columns = ["branchName", "totalCommits"]
-    branch_count_graph = px.bar(branch_counts, x=branch_counts.branchName, y=branch_counts.totalCommits, color=branch_counts.branchName, labels={"branchName":"","totalCommits":""})
-    branch_count_graph.update_layout(
-        showlegend = False,
-        margin = dict(l=1,r=1,t=1,b=1),
-        height=220
-    )
-    branch_graph_col.plotly_chart(branch_count_graph, use_container_width=True)
-    #-------------------------
-
-    #-------------------------
-    #COMMIT PANDAS TABLE
+    collaborator_graph_col.plotly_chart(authorFig, use_container_width=True)   
+    #-------
+    
+    #-------
+    #COMMIT PANDAS TABLE 🔲
     st.subheader("Commit Activity Timeline 🕒")
     #created at parameter to dataframe with counts
-    cdate = pd.to_datetime(scommits["createdAt"]).dt.date.value_counts().reset_index().sort_values("index")
+    cdate = pd.to_datetime(commits["createdAt"]).dt.date.value_counts().reset_index().sort_values("index")
     #date range to fill null dates.
     null_days = pd.date_range(start=cdate["index"].min(), end=cdate["index"].max())
     #add null days to table
@@ -191,15 +223,16 @@ with graphs:
     cdate.columns = ["date", "count"]
     #redate indexed dates
     cdate["date"] = pd.to_datetime(cdate["date"]).dt.date
-    #------------------------
+    #-------
 
-    #------------------------
-    #COMMIT ACTIVITY LINE CHART
+    #-------
+    #COMMIT ACTIVITY LINE CHART📈
     #line chart
     fig = px.line(cdate, x=cdate["date"], y=cdate["count"], markers =True)
     #recolor line
     
     #Show Chart
     st.plotly_chart(fig, use_container_width=True)
-    #------------------------
+    #-------
 
+#--------------------------
